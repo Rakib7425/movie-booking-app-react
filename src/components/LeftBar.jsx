85a3bbbcd8e3c710d04ee9e61d91637eb2dce7b8
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-06-25 12:54:31 +0530

    modified:   src/components/LeftBar.jsx

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -2,15 +2,100 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const LeftBar = () => {
+    let genres = [
+        {
+            "id": 28,
+            "name": "Action"
+        },
+        {
+            "id": 12,
+            "name": "Adventure"
+        },
+        {
+            "id": 16,
+            "name": "Animation"
+        },
+        {
+            "id": 35,
+            "name": "Comedy"
+        },
+        {
+            "id": 80,
+            "name": "Crime"
+        },
+        {
+            "id": 99,
+            "name": "Documentary"
+        },
+        {
+            "id": 18,
+            "name": "Drama"
+        },
+        {
+            "id": 10751,
+            "name": "Family"
+        },
+        {
+            "id": 14,
+            "name": "Fantasy"
+        },
+        {
+            "id": 36,
+            "name": "History"
+        },
+        {
+            "id": 27,
+            "name": "Horror"
+        },
+        {
+            "id": 10402,
+            "name": "Music"
+        },
+        {
+            "id": 9648,
+            "name": "Mystery"
+        },
+        {
+            "id": 10749,
+            "name": "Romance"
+        },
+        {
+            "id": 878,
+            "name": "Science Fiction"
+        },
+        {
+            "id": 10770,
+            "name": "TV Movie"
+        },
+        {
+            "id": 53,
+            "name": "Thriller"
+        },
+        {
+            "id": 10752,
+            "name": "War"
+        },
+        {
+            "id": 37,
+            "name": "Western"
+        }
+    ]
     return (
         <>
-            <div className="leftbar w-[10vw] min-h-full border-2 border-red-400 mr-2">
+            <div className="leftbar max-w-fit min-h-full border-2 border-red-400 mr-2">
                 <h2 className='text-center text-2xl'>Genres</h2>
                 <hr />
                 <ul className='text-left text-[1.2rem] mx-4'>
-                    <li>
-                        <Link >Home</Link>
-                    </li>
+                    {
+                        Array.isArray(genres) &&
+                        genres.map((item, index) => {
+                            return (
+                                <li className='p-2'>
+                                    <Link key={index}>{item.name}</Link>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
         </>
