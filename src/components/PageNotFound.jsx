c02d91c970df6c8665c650927ecd1581a95725a4
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-06-28 18:32:45 +0530

    modified:   src/components/PageNotFound.jsx 	modified:   tailwind.config.js

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { Box, Button, Container, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/firebase/auth';
 
 export default function Error() {
+    const { authUser } = useAuth();
+
     return (
         <Box
             sx={{
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                minHeight: '100vh'
+                minHeight: '88vh',
             }}
+
         >
             <Container maxWidth="md">
                 <Grid container spacing={2}>
@@ -22,17 +26,17 @@ export default function Error() {
                         <Typography variant="h6">
                             The page you’re looking for doesn’t exist.
                         </Typography>
-                        <Link to={'/'}> <Button variant="contained">Back Home </Button> </Link>
+                        <Link to={authUser ? '/' : '/login'}> <Button variant="contained"  >Back Home </Button> </Link>
                     </Grid>
                     <Grid xs={6}>
                         <img
                             src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
                             alt=""
-                            width={500} height={250}
+                            width={550} height={300}
                         />
                     </Grid>
                 </Grid>
             </Container>
-        </Box>
+        </Box >
     );
 }
