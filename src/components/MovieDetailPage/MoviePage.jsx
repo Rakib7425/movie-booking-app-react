26ccd34a6b9b195d79612c6d944138e5da3a6249
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-07-05 18:54:12 +0530

    modified:   public/index.html 	modified:   src/components/Explore/Explore.module.css 	modified:   src/components/MovieDetailPage/MoviePage.jsx

--- src/components/MovieDetailPage/MoviePage.jsx
+++ src/components/MovieDetailPage/MoviePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { getMovieDetails, getSimilarMovies } from "../../api/movies";
+import { getMovieDetails, getSimilarMovies, getMovieImages } from "../../api/movies";
 import MovieCard from "../MovieCard/MovieCard";
 import styles from "./MoviePage.module.css";
 
@@ -10,6 +10,11 @@ function MoviePage() {
   const params = useParams();
   const movieId = params.movieId;
   const [movie, setMovie] = useState({});
+
+
+  const [images, setImages] = useState([]);
+
+
   const [similarMovies, setSimilarMovies] = useState([]);
   const price = Math.floor(Math.random() * 100 + 200);
 
@@ -27,65 +32,92 @@ function MoviePage() {
     });
   };
 
+  // const fetchMovieImages = () => {
+  //   getMovieImages(movieId).then((res) => {
+  //     if (!res) return;
+  //     setImages(res.backdrops);
+  //     console.log(res.backdrops);
+  //   });
+  // };
+
   useEffect(() => {
     fetchMovieDetails();
     fetchSimilarMovies();
-
+    // fetchMovieImages()
     // eslint-disable-next-line
   }, [movieId]);
 
 
   return (
-    <div className={styles.container}>
-      <div className={`${styles.main} sm:flex-col md:flex-col xl:flex-row`}>
-        <img className={!movie?.backdrop_path ? 'max-w-80 max-h-80 w-80 h-80 my-auto' : ''} src={!movie?.backdrop_path ? 'https://w7.pngwing.com/pngs/116/765/png-transparent-clapperboard-computer-icons-film-movie-poster-angle-text-logo-thumbnail.png' : `${imagePrefixUrl}${movie.backdrop_path}`} alt="aMovieImg" />
-        <div className={styles.details}>
-          <label>Title</label>
-          <div className={styles.title}>{movie?.title}</div>
-          <div className={styles.sub}>{movie?.tagline}</div>
-          <label>Story</label>
-          <div className={styles.desc}>{movie?.overview}</div>
+    <>
+      <div className={styles.container}>
+        <div className={`${styles.main} flex flex-col md:flex-col xl:flex-row`}>
+          <div className="min-h-80 md:min-w-[45vw] flex xs:flex-col sm:flex-col md:flex-col xl:flex-row ">
 
-          <div className="mt-4 mb-2 text-lg flex justify-between">
-            <span
-              data-tooltip-target="money"
-              className="mb-2 text-green-500 transition-colors hover:border-pink-500/10 hover:!opacity-100 group-hover:opacity-70"
-            >
-              Run time : <span >{movie?.runtime} (Mins) </span>
-            </span>
-            <span
-              data-tooltip-target="money"
-              className="mb-2 text-green-500 transition-colors hover:border-pink-500/10 hover:!opacity-100 group-hover:opacity-70"
-            >
-              Language: <span className='uppercase'>{movie?.original_language}</span>
-            </span>
-            <span
-              data-tooltip-target="money"
-              className="pl-2 mb-2 text-green-500 transition-colors hover:border-pink-500/10 hover:!opacity-100 group-hover:opacity-70"
-            >
-              Price: {`${price}`}
-            </span>
+            <img className={!movie?.backdrop_path ? 'max-w-80 max-h-80 w-80 h-80 my-auto ' : ''} src={!movie?.backdrop_path ? 'https://w7.pngwing.com/pngs/116/765/png-transparent-clapperboard-computer-icons-film-movie-poster-angle-text-logo-thumbnail.png' : `${imagePrefixUrl}${movie.backdrop_path}`} alt="aMovieImg" />
 
           </div>
+          <div className={styles.details}>
+            <label>Title</label>
+            <div className={styles.title}>{movie?.title}</div>
+            <div className={styles.sub}>{movie?.tagline}</div>
+            <label>Story</label>
+            <div className={styles.desc}>{movie?.overview}</div>
 
-          <Link to={`/movie/${movieId}/${price}`}
-            className="z-[2] inline-block rounded bg-blue-700 px-6 pb-2 pt-2.5 text-xs text-center font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:z-[3]  focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] mx-1"
+            <div className="mt-4 mb-2 text-lg flex justify-between">
+              <span
+                data-tooltip-target="money"
+                className="mb-2 text-green-500 transition-colors hover:border-pink-500/10 hover:!opacity-100 group-hover:opacity-70"
+              >
+                Run time : <span >{movie?.runtime} (Mins) </span>
+              </span>
+              <span
+                data-tooltip-target="money"
+                className="mb-2 text-green-500 transition-colors hover:border-pink-500/10 hover:!opacity-100 group-hover:opacity-70"
+              >
+                Language: <span className='uppercase'>{movie?.original_language}</span>
+              </span>
+              <span
+                data-tooltip-target="money"
+                className="pl-2 mb-2 text-green-500 transition-colors hover:border-pink-500/10 hover:!opacity-100 group-hover:opacity-70"
+              >
+                Price: {`${price}`}
+              </span>
 
-            type="button"
-            id="sign-btn">
-            Book Now
-          </Link>
+            </div>
+
+            <Link to={`/movie/${movieId}/${price}`}
+              className="z-[2] inline-block rounded bg-blue-700 px-6 pb-2 pt-2.5 text-xs text-center font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:z-[3]  focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] mx-1"
+
+              type="button"
+              id="sign-btn">
+              Book Now
+            </Link>
+          </div>
+        </div>
+        <div className={styles.similar}>
+          <div className={styles.title}>Similar movies</div>
+          <div className={styles.movies}>
+            {similarMovies.map((item) => (
+              <MovieCard movie={item} key={item.id} />
+            ))}
+          </div>
         </div>
       </div>
-      <div className={styles.similar}>
-        <div className={styles.title}>Similar movies</div>
-        <div className={styles.movies}>
-          {similarMovies.map((item) => (
-            <MovieCard movie={item} key={item.id} />
-          ))}
-        </div>
+
+      <div className="new">
+        {
+          images && images.map((item, index) => {
+            return (
+              <div className="img">
+                {/* <img src={item.file_path} alt="item.xss" /> */}
+                <img src={`${imagePrefixUrl}${item.file_path}`} alt="vf" />
+              </div>
+            )
+          })
+        }
       </div>
-    </div>
+    </>
   );
 }
 
