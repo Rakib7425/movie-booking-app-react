b76ff638ccd9e803c53fa3dd1e1c6618fbb8f89f
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-07-10 01:38:59 +0530

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -109,3 +109,146 @@ const Header = () => {
 export default Header;
 
 // export {inputData}
+
+
+
+/**
+ * import React, { useEffect, useState } from 'react';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
+import SearchMovie from './SearchMovie';
+import { useAuth } from '../contexts/firebase/auth';
+
+const Header = () => {
+  const [inputData, setInputData] = useState('');
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const { authUser, signOut, isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authUser) {
+      navigate('/login');
+    }
+    // eslint-disable-next-line
+  }, [authUser, isLoading]);
+
+  const navLinksStyles = ({ isActive }) => {
+    return {
+      textDecoration: isActive ? 'underline' : 'none',
+      color: isActive ? 'yellow' : '',
+    };
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <header className="flex justify-between items-center my-4">
+        <div className="logo flex">
+          <Link to={authUser ? '/' : '/login'}>
+            <h1 className="text-xl hover:text-gray-300">My-Movie-App</h1>
+          </Link>
+          <h1 className="text-xl ml-4 text-green-400">
+            <NavLink to={authUser && '/user-profile'} style={navLinksStyles}>
+              {authUser ? `Hello-  ${authUser.Name}` : <span className="text-red-500">Login to Access</span>}
+            </NavLink>
+          </h1>
+        </div>
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            className="relative m-0 -mr-0.5 block w-[250px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-white outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-blue focus:text-white focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none"
+            placeholder="Search Movie..."
+            aria-label="Search"
+            aria-describedby="Search"
+            value={inputData}
+            onChange={(e) => setInputData(e.target.value)}
+          />
+          <Link
+            className="z-[2] inline-block rounded-r bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:z-[3] focus:bg-blue-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] mx-1"
+            type="button"
+            id="Search"
+            disabled
+          >
+            Search
+          </Link>
+          <div className={authUser ? 'hidden' : ''}>
+            <Link
+              to="/login"
+              className="z-[2] inline-block rounded bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:z-[3] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] mx-1"
+              type="button"
+              id="sign-btn"
+            >
+              Login
+            </Link>
+          </div>
+          <div className={!authUser ? 'hidden' : ''}>
+            <Link
+              className="z-[2] inline-block rounded bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:z-[3] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] mx-1"
+              type="button"
+              id="sign-btn"
+              onClick={signOut}
+            >
+              Logout
+            </Link>
+          </div>
+          <button
+            className="md:hidden ml-4 text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500"
+            onClick={toggleMenu}
+          >
+            <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path
+                className={`${isMenuOpen ? 'hidden' : 'block'}`}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+              <path
+                className={`${isMenuOpen ? 'block' : 'hidden'}`}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </header>
+      {isMenuOpen && (
+        <nav className="md:hidden">
+          <ul className="flex flex-col space-y-4">
+            <li>
+              <NavLink
+                style={navLinksStyles}
+                to="/explore"
+                className={authUser ? '' : 'hidden'}
+                onClick={() => setMenuOpen(false)}
+              >
+                Explore Movies
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={navLinksStyles}
+                to="user/bookings"
+                className={authUser ? '' : 'hidden'}
+                onClick={() => setMenuOpen(false)}
+              >
+                My Bookings
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      )}
+
+      {inputData ? <SearchMovie inputData={inputData} setInputData={setInputData} /> : ''}
+    </>
+  );
+};
+
+export default Header;
+
+ */
