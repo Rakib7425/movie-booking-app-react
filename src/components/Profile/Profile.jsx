9876ed27bb7137c57b88c00220ee5dd63878fb9b
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-07-12 00:58:55 +0530

    modified:   src/components/Profile/Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-
+import { useAuth } from '../../contexts/firebase/auth'
 const Profile = () => {
+    const user = useAuth()
+    const fetchedUser = user.authUser;
+    console.log(user.authUser);
     return (
         <>Profile
 
@@ -9,7 +12,6 @@ const Profile = () => {
                 Working in this page ~TODO...........
             </h1>
             <>
-
                 <section className="h-full">
                     <div className="container px-6 pt-20">
                         <div
@@ -26,43 +28,41 @@ const Profile = () => {
                             <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
                                 <form onSubmit={(e) => e.preventDefault()}>
 
-                                    {/* <!-- Name input --> */}
+                                    {/* <!-- User ID --> */}
                                     <div className="relative mb-6" data-te-input-wrapper-init>
-                                        <div className='flex gap-2 '>
-                                            <div>
-                                                <label className=''
-                                                    htmlFor="fname"
-                                                >First Name
-                                                </label>
-                                                <input
-                                                    className="bg-gray-200 mt-2 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-800 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 transition-all duration-200"
 
-                                                    type="text"
-                                                    id="fName"
-                                                    placeholder="First Name"
-                                                    required
-                                                    minLength={2}
-                                                // value={fName}
-                                                // onChange={(e) => setfName(e.target.value)} 
-                                                />
-                                            </div>
-                                            <div>
-                                                <label className=''
-                                                    htmlFor="lName"
-                                                >Last Name
-                                                </label>
-                                                <input
-                                                    className="bg-gray-200 mt-2 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-800 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 transition-all duration-200"
-                                                    type="text"
-                                                    id="lname"
-                                                    placeholder="Last Name"
-                                                // value={lName}
-                                                // onChange={(e) => setlName(e.target.value)}
-                                                />
-                                            </div>
-                                        </div>
+                                        <label className=''
+                                            htmlFor="userId"
+                                        >User ID
+                                        </label>
+                                        <input
+                                            className="bg-gray-200 mt-2 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-800 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 transition-all duration-200"
+
+                                            type="text"
+                                            id="userId"
+                                            placeholder="user Id"
+                                            disabled
+                                            defaultValue={fetchedUser?.userId}
+                                        />
                                     </div>
-                                    {/* <!-- Email input --> */}
+
+                                    <div className="relative mb-6" data-te-input-wrapper-init>
+                                        <label className=''
+                                            htmlFor="fullName"
+                                        >Full Name
+                                        </label>
+                                        <input
+                                            type="text"
+                                            className="bg-gray-200 mt-2 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-800 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 transition-all duration-200"
+                                            id="fullName"
+                                            placeholder="Full Name"
+                                            required
+                                            minLength={2}
+                                            defaultValue={fetchedUser?.Name}
+                                        // value={email}
+                                        // onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                    </div>
                                     <div className="relative mb-6" data-te-input-wrapper-init>
                                         <label className=''
                                             htmlFor="email"
@@ -75,6 +75,7 @@ const Profile = () => {
                                             placeholder="Email address"
                                             required
                                             minLength={2}
+                                            defaultValue={fetchedUser?.Email}
                                         // value={email}
                                         // onChange={(e) => setEmail(e.target.value)}
                                         />
