6cab12e3708de49b34eb8b7d54e405369998ce19
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-07-12 00:01:27 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ import Profile from './components/Profile/Profile';
 const App = () => {
   const [data, setData] = useState([])
   const [page, setPage] = useState(1)
-  // const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=c43eafb6cfde3357615b65d291332480&page=${page}`
-  const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=c43eafb6cfde3357615b65d291332480&page=${page}`
+  const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=c43eafb6cfde3357615b65d291332480&page=${page}`
+  // const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=c43eafb6cfde3357615b65d291332480&page=${page}`
   useEffect(() => {
     getData();
 
