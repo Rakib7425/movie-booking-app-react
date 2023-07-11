6912ab9d8f19a18352047774934ce81e58d947af
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-07-12 00:59:23 +0530

    modified:   src/components/Profile/Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -21,7 +21,7 @@ const Profile = () => {
                                 <img
                                     src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
                                     className="w-full"
-                                    alt="Phone" />
+                                    alt="ImgG" />
                             </div>
 
                             {/* <!-- Right column container with form --> */}
